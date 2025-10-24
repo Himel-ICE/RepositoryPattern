6649dce5a3d88cefc3802771e08ea92ec1ef1f79
github.com/Himel-ICE <islam9himel@gmail.com>
2025-10-24 23:56:08 +0600

    add DbContext

---
+++ RepositoryPattern/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Domain.Entities;
+
+namespace RepositoryPattern.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- RepositoryPattern/obj/Debug/net8.0/RepositoryPattern.AssemblyInfo.cs
+++ RepositoryPattern/obj/Debug/net8.0/RepositoryPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aaad6fdd631e7f3ac5e04fb1ff3373356a9b3f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6cc44d0dac482de4898e252645e824712c22ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
