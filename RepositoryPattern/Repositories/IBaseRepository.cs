f748dc5aae1deefd73fae65299b8102191e2844c
github.com/Himel-ICE <islam9himel@gmail.com>
2025-10-25 01:13:19 +0600

    add repository

---
+++ RepositoryPattern/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace RepositoryPattern.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task<List<T>> GetAll(params Expression<Func<T, object>>[] includes);
+        Task<T?> GetById(Guid id, params Expression<Func<T, object>>[] includes);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(Guid id);
+    }
+}
--- RepositoryPattern/obj/Debug/net8.0/RepositoryPattern.AssemblyInfo.cs
+++ RepositoryPattern/obj/Debug/net8.0/RepositoryPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6cc44d0dac482de4898e252645e824712c22ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6649dce5a3d88cefc3802771e08ea92ec1ef1f79")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
